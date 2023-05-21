e7b165640ae018c63f1f7cc69b0274f93b448058
MANJU <manju>
2023-05-21 16:47:36 +0530

    commit and push

---
+++ src/test/java/MevenDemo/PracticeMevenTest.java
@@ -0,0 +1,13 @@
+package MevenDemo;
+
+import org.testng.annotations.Test;
+
+public class PracticeMevenTest {
+	
+	@Test
+	public void practice()
+	{
+		System.out.println("Hello AM GOOD");
+	}
+
+}
